f43a4bd12fbaed33ccb765c5c74ac493abe431cf
Bautista Frigolé <bautistafrigole@gmail.com>
2024-04-16 19:54:14 -0300

    Fixed subscriber.py

--- tp4/subscriber.py
+++ tp4/subscriber.py
@@ -1,11 +1,24 @@
 import zmq
 
-context = zmq.Context()
-socket = context.socket(zmq.SUB)
-socket.connect("tcp://localhost:5555")
+connect_to = "tcp://localhost:3000"
+topic_to_subscribe = b'sd are fun!'
 
-socket.setsockopt_string(zmq.SUBSCRIBE, "")
+def main() -> None:
+    ctx = zmq.Context()
+    s = ctx.socket(zmq.SUB)
+    s.connect(connect_to)
+    s.setsockopt(zmq.SUBSCRIBE, topic_to_subscribe)
 
-while True:
-    message = socket.recv_string()
-    print("Mensaje recibido:", message)
+    print(f"Subscribed to topic: {topic_to_subscribe.decode('utf8')}")
+
+    try:
+        while True:
+            topic, msg_body = s.recv_multipart()
+            print(f"Received message: Topic: {topic.decode('utf8')}, Message: {msg_body.decode('utf8')}")
+    except KeyboardInterrupt:
+        pass
+
+    print("Done.")
+
+if __name__ == "__main__":
+    main()
